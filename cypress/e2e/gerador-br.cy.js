8867aba4c3e27503619d791ad1faadd84a087ef6
marcelo-lourenco <mlourenco.info@gmail.com>
2025-05-13 23:23:50 -0300

    test(validation): add scenario

--- cypress/e2e/gerador-br.cy.js
+++ cypress/e2e/gerador-br.cy.js
@@ -1,134 +1,276 @@
 import { gerar } from 'gerador-br';
 
-describe('Gerador BR Formulário', () => {
+describe('Gerador BR Formulário - Conversão Playwright para Cypress', () => {
   const url = 'https://marcelo-lourenco.github.io/gerador-br/formulario-para-teste/';
+
   beforeEach(() => {
     cy.visit(url);
   });
 
   it('Validates data generation WITHOUT MASK', () => {
-
-    // Pessoa
     const nomeCompleto = gerar.nome.aleatorioCompleto();
-    cy.get('[data-testid="input-nome"]').type(nomeCompleto);
-    cy.get('[data-testid="input-pai"]').type(gerar.nome.paiCompleto());
-    cy.get('[data-testid="input-mae"]').type(gerar.nome.maeCompleto());
-    cy.get('[data-testid="input-apelido"]').type(gerar.apelido());
-    cy.get('[data-testid="input-escolaridade"]').type(gerar.escolaridade());
-    cy.get('[data-testid="input-profissao"]').type(gerar.profissao());
-    cy.get('[data-testid="input-cor-pele"]').type(gerar.corPele());
-    cy.get('[data-testid="input-genero"]').type(gerar.genero());
-    cy.get('[data-testid="input-orientacao-sexual"]').type(gerar.orientacaoSexual());
-    cy.get('[data-testid="input-identidade-genero"]').type(gerar.identidadeGenero());
-    cy.get('[data-testid="input-tipo-sanguineo"]').type(gerar.tipoSanguineo());
-    cy.get('[data-testid="input-data-nascimento"]').type(gerar.dataNascimento());
+    cy.get('#fldNome').type(nomeCompleto);
+    cy.get('#fldPai').type(gerar.nome.paiCompleto());
+    cy.get('#fldMae').type(gerar.nome.maeCompleto());
+    cy.get('#fldApelido').type(gerar.apelido());
+    cy.get('#fldEscolaridade').type(gerar.escolaridade());
+    cy.get('#fldProfissao').type(gerar.profissao());
+    cy.get('#fldCorPele').type(gerar.corPele());
+    cy.get('#fldGenero').type(gerar.genero());
+    cy.get('#fldOrientacaoSexual').type(gerar.orientacaoSexual());
+    cy.get('#fldIdentidadeGenero').type(gerar.identidadeGenero());
+    cy.get('#fldTipoSanguineo').type(gerar.tipoSanguineo());
+    cy.get('#fldDataNascimento').type(gerar.dataNascimento());
 
     // Documentos PJ
-    cy.get('[data-testid="input-cnpj"]').type(gerar.cnpj());
-    cy.get('[data-testid="input-inscricao-estadual"]').type(gerar.inscricaoEstadual() ?? '');
+    cy.get('#fldCnpj').type(gerar.cnpj());
+    cy.get('#fldInscricaoEstadual').type(gerar.inscricaoEstadual() ?? '');
+    cy.get('#fldNomeEmpresa').type(gerar.nomeEmpresa("BR"));
 
     // Documentos PF
-    cy.get('[data-testid="input-cpf"]').type(gerar.cpf());
-    cy.get('[data-testid="input-cnh"]').type(gerar.cnh());
-    cy.get('[data-testid="input-cnh-categoria"]').type(gerar.cnhCategoria());
-    cy.get('[data-testid="input-rg"]').type(gerar.rg());
-    cy.get('[data-testid="input-cns"]').type(gerar.cns());
-    cy.get('[data-testid="input-pis"]').type(gerar.pis());
-    cy.get('[data-testid="input-titulo-eleitor"]').type(gerar.tituloEleitor());
-    cy.get('[data-testid="input-passaporte"]').type(gerar.passaporte());
-    cy.get('[data-testid="input-certidao-nascimento"]').type(gerar.certidao.nascimento());
-    cy.get('[data-testid="input-certidao-casamento"]').type(gerar.certidao.casamento());
-    cy.get('[data-testid="input-certidao-obito"]').type(gerar.certidao.obito());
+    cy.get('#fldCpf').type(gerar.cpf());
+    cy.get('#fldCnh').type(gerar.cnh());
+    cy.get('#fldCnhCategoria').type(gerar.cnhCategoria());
+    cy.get('#fldRg').type(gerar.rg());
+    cy.get('#fldCns').type(gerar.cns());
+    cy.get('#fldPis').type(gerar.pis());
+    cy.get('#fldTituloEleitor').type(gerar.tituloEleitor());
+    cy.get('#fldPassaporte').type(gerar.passaporte());
+    cy.get('#fldCertidaoNascimento').type(gerar.certidao.nascimento());
+    cy.get('#fldCertidaoCasamento').type(gerar.certidao.casamento());
+    cy.get('#fldCertidaoObito').type(gerar.certidao.obito());
 
     // Contatos
-    cy.get('[data-testid="input-email"]').type(gerar.email(nomeCompleto));
-    cy.get('[data-testid="input-ddd"]').type(gerar.ddd());
-    cy.get('[data-testid="input-celular"]').type(gerar.celular());
-    cy.get('[data-testid="input-telefone"]').type(gerar.telefone());
+    cy.get('#fldEmail').type(gerar.email(nomeCompleto));
+    cy.get('#fldDdd').type(gerar.ddd());
+    cy.get('#fldCelular').type(gerar.celular());
+    cy.get('#fldTelefone').type(gerar.telefone());
 
     // Endereços
     const endereco = gerar.endereco();
-    cy.get('[data-testid="input-logradouro"]').type(endereco.logradouro);
-    cy.get('[data-testid="input-numero"]').type(endereco.numero);
-    cy.get('[data-testid="input-complemento"]').type(endereco.complemento ? endereco.complemento : '-');
-    cy.get('[data-testid="input-bairro"]').type(endereco.bairro);
-    cy.get('[data-testid="input-localidade"]').type(endereco.localidade);
-    cy.get('[data-testid="input-estado"]').type(endereco.estado);
-    cy.get('[data-testid="input-cep"]').type(endereco.cep);
+    cy.get('#fldLogradouro').type(endereco.logradouro);
+    cy.get('#fldNumero').type(endereco.numero);
+    cy.get('#fldComplemento').type(endereco.complemento ? endereco.complemento : '-');
+    cy.get('#fldBairro').type(endereco.bairro);
+    cy.get('#fldLocalidade').type(endereco.localidade);
+    cy.get('#fldEstado').type(endereco.estado);
+    cy.get('#fldCep').type(endereco.cep);
 
     // Cartão de Crédito
     const cartaoCredito = gerar.cartaoCredito();
-    console.log("cartaoCredito",cartaoCredito);
-    cy.get('[data-testid="input-cartao-credito"]').type(cartaoCredito.numero);
-    cy.get('[data-testid="input-bandeira"]').type(cartaoCredito.bandeira);
-    cy.get('[data-testid="input-nome-titular"]').type(cartaoCredito.nomeTitular);
-    cy.get('[data-testid="input-cvv"]').type(cartaoCredito.cvv);
-    cy.get('[data-testid="input-data-expiracao"]').type(cartaoCredito.dataExpiracao);
+    cy.get('#fldCartaoCredito').type(cartaoCredito.numero);
+    cy.get('#fldBandeira').type(cartaoCredito.bandeira);
+    cy.get('#fldNomeTitular').type(cartaoCredito.nomeTitular);
+    cy.get('#fldCvv').type(cartaoCredito.cvv);
+    cy.get('#fldDataExpiracao').type(cartaoCredito.dataExpiracao);
 
     // Conta Bancária
     const cb = gerar.contaBancaria();
-    cy.get('[data-testid="input-codigo-banco-conta"]').type(cb.codigoBanco);
-    cy.get('[data-testid="input-nome-banco-conta"]').type(cb.nomeBanco);
-    cy.get('[data-testid="input-agencia"]').type(cb.agencia);
-    cy.get('[data-testid="input-agencia-dv"]').type(cb.agenciaDv ? cb.agenciaDv : '-');
-    cy.get('[data-testid="input-conta"]').type(cb.conta);
-    cy.get('[data-testid="input-conta-dv"]').type(cb.contaDv ? cb.contaDv : '-');
+    cy.get('#fldCodigoBanco').type(cb.codigoBanco);
+    cy.get('#fldNomeBanco').type(cb.nomeBanco);
+    cy.get('#fldAgencia').type(cb.agencia);
+    cy.get('#fldAgenciaDv').type(cb.agenciaDv ? cb.agenciaDv : '-');
+    cy.get('#fldConta').type(cb.conta);
+    cy.get('#fldContaDv').type(cb.contaDv ? cb.contaDv : '-');
 
     // Banco
     const banco = gerar.banco();
-    cy.get('[data-testid="input-banco-codigo"]').type(banco.codigoBanco);
-    cy.get('[data-testid="input-banco-nome"]').type(banco.nomeBanco);
-    cy.get('[data-testid="input-razao-social"]').type(banco.razaoSocial);
-    cy.get('[data-testid="input-ispb"]').type(banco.ispb);
+    cy.get('#fldBancoCodigo').type(banco.codigoBanco);
+    cy.get('#fldBancoNome').type(banco.nomeBanco);
+    cy.get('#fldRazaoSocial').type(banco.razaoSocial);
+    cy.get('#fldIspb').type(banco.ispb);
 
     // Veículo
-    cy.get('[data-testid="input-placa-antiga"]').type(gerar.placaAntiga());
-    cy.get('[data-testid="input-placa-mercosul"]').type(gerar.placaMercosul());
-    cy.get('[data-testid="input-renavam"]').type(gerar.renavam());
+    cy.get('#fldPlacaAntiga').type(gerar.placaAntiga());
+    cy.get('#fldPlacaMercosul').type(gerar.placaMercosul());
+    cy.get('#fldRenavam').type(gerar.renavam());
 
     // Lorem Ipsum
-    cy.get('[data-testid="input-palavra"]').type(gerar.palavra());
-    cy.get('[data-testid="textarea-sentenca"]').type(gerar.sentenca());
-    cy.get('[data-testid="textarea-paragrafos"]').type(gerar.paragrafo());
+    cy.get('#fldPalavra').type(gerar.palavra());
+    cy.get('#fldSentenca').type(gerar.sentenca());
+    cy.get('#fldParagrafos').type(gerar.paragrafo());
 
     cy.contains('button', 'Enviar').click();
     cy.wait(3000);
   });
 
   it('Validates data generation WITH MASK', () => {
-
     // Documentos PJ
-    cy.get('[data-testid="input-cnpj"]').type(gerar.cnpj(true));
+    cy.get('#fldCnpj').type(gerar.cnpj(true));
 
     // Documentos PF
-    cy.get('[data-testid="input-cpf"]').type(gerar.cpf(true));
-    cy.get('[data-testid="input-rg"]').type(gerar.rg(true));
-    cy.get('[data-testid="input-cns"]').type(gerar.cns(true));
-    cy.get('[data-testid="input-pis"]').type(gerar.pis(true));
-    cy.get('[data-testid="input-titulo-eleitor"]').type(gerar.tituloEleitor(true));
-    cy.get('[data-testid="input-certidao-nascimento"]').type(gerar.certidao.nascimento(true));
-    cy.get('[data-testid="input-certidao-casamento"]').type(gerar.certidao.casamento(true));
-    cy.get('[data-testid="input-certidao-obito"]').type(gerar.certidao.obito(true));
+    cy.get('#fldCpf').type(gerar.cpf(true));
+    cy.get('#fldRg').type(gerar.rg(true));
+    cy.get('#fldCns').type(gerar.cns(true));
+    cy.get('#fldPis').type(gerar.pis(true));
+    cy.get('#fldTituloEleitor').type(gerar.tituloEleitor(true));
+    cy.get('#fldCertidaoNascimento').type(gerar.certidao.nascimento(true));
+    cy.get('#fldCertidaoCasamento').type(gerar.certidao.casamento(true));
+    cy.get('#fldCertidaoObito').type(gerar.certidao.obito(true));
 
     // Contato
-    cy.get('[data-testid="input-email"]').type(gerar.email(gerar.apelido()));
-    cy.get('[data-testid="input-ddd"]').type(gerar.ddd());
-    cy.get('[data-testid="input-celular"]').type(gerar.celular(true));
-    cy.get('[data-testid="input-telefone"]').type(gerar.telefone(true));
+    cy.get('#fldEmail').type(gerar.email(gerar.apelido()));
+    cy.get('#fldDdd').type(gerar.ddd());
+    cy.get('#fldCelular').type(gerar.celular(true));
+    cy.get('#fldTelefone').type(gerar.telefone(true));
 
     // Endereço
     const endereco = gerar.endereco(true);
-    cy.get('[data-testid="input-cep"]').type(endereco.cep);
+    cy.get('#fldCep').type(endereco.cep);
 
     // Cartão de Crédito
     const cartaoCredito = gerar.cartaoCredito(true);
-    cy.get('[data-testid="input-cartao-credito"]').type(cartaoCredito.numero);
+    cy.get('#fldCartaoCredito').type(cartaoCredito.numero);
 
     // Veículo
-    cy.get('[data-testid="input-placa-antiga"]').type(gerar.placaAntiga(true));
-    cy.get('[data-testid="input-placa-mercosul"]').type(gerar.placaMercosul(true));
+    cy.get('#fldPlacaAntiga').type(gerar.placaAntiga(true));
+    cy.get('#fldPlacaMercosul').type(gerar.placaMercosul(true));
+
+    // Lorem Ipsum
+    cy.get('#fldPalavra').type(gerar.palavra());
+    cy.get('#fldSentenca').type(gerar.sentenca());
+    cy.get('#fldParagrafos').type(gerar.paragrafo());
 
     cy.contains('button', 'Enviar').click();
     cy.wait(3000);
+  });
+
+  it('Validates data generation with MASK = false and with informed state', () => {
+    const estado = 'SP';
+
+    cy.get('#fldCpf').type(gerar.cpf(false, estado));
+    cy.get('#fldCnpj').type(gerar.cnpj(false));
+    cy.get('#fldRg').type(gerar.rg(false));
+    cy.get('#fldCns').type(gerar.cns(false, 'beneficiario'));
+    cy.get('#fldPis').type(gerar.pis(false));
+    cy.get('#fldTituloEleitor').type(gerar.tituloEleitor(false, estado));
+    cy.get('#fldCertidaoNascimento').type(gerar.certidao.nascimento(false));
+    cy.get('#fldCertidaoCasamento').type(gerar.certidao.casamento(false));
+    cy.get('#fldCertidaoObito').type(gerar.certidao.obito(false));
+    cy.get('#fldCelular').type(gerar.celular(false, estado));
+    cy.get('#fldTelefone').type(gerar.telefone(false, estado));
+    cy.get('#fldCep').type(gerar.cep(false, estado));
+    cy.get('#fldPlacaAntiga').type(gerar.placaAntiga(false));
+    cy.get('#fldPlacaMercosul').type(gerar.placaMercosul(false));
+    cy.get('#fldCartaoCredito').type(gerar.cartaoCredito(false).numero);
+
+    cy.contains('button', 'Enviar').click();
+    cy.wait(2000);
+  });
+
+  it('Validates data generation with MASK = true and with informed state', () => {
+    const estado = 'SP';
+
+    cy.get('#fldCpf').type(gerar.cpf(true, estado));
+    cy.get('#fldCnpj').type(gerar.cnpj(true));
+    cy.get('#fldRg').type(gerar.rg(true));
+    cy.get('#fldCns').type(gerar.cns(true, 'profissional'));
+    cy.get('#fldPis').type(gerar.pis(true));
+    cy.get('#fldTituloEleitor').type(gerar.tituloEleitor(true, estado));
+    cy.get('#fldCertidaoNascimento').type(gerar.certidao.nascimento(true));
+    cy.get('#fldCertidaoCasamento').type(gerar.certidao.casamento(true));
+    cy.get('#fldCertidaoObito').type(gerar.certidao.obito(true));
+    cy.get('#fldCelular').type(gerar.celular(true, estado));
+    cy.get('#fldTelefone').type(gerar.telefone(true, estado));
+    cy.get('#fldCep').type(gerar.cep(true, estado));
+    cy.get('#fldPlacaAntiga').type(gerar.placaAntiga(true));
+    cy.get('#fldPlacaMercosul').type(gerar.placaMercosul(true));
+    cy.get('#fldCartaoCredito').type(gerar.cartaoCredito(true).numero);
+
+    cy.contains('button', 'Enviar').click();
+    cy.wait(2000);
+  });
+
+  it('Validates data generation according to variables (mask, gender, sexual orientation, state and bank)', () => {
+    const mascara = true;
+    const genero = 'f';
+    const nomeCompleto = gerar.nome.femininoCompleto();
+    const estado = "PR";
+    const banco = '237';
+    const orientacaoSexual = gerar.orientacaoSexual();
+
+    cy.get('#fldNome').type(nomeCompleto);
+    cy.get('#fldPai').type(gerar.nome.paiCompleto());
+    cy.get('#fldMae').type(gerar.nome.maeCompleto());
+    cy.get('#fldApelido').type(gerar.apelido());
+    cy.get('#fldEscolaridade').type(gerar.escolaridade());
+    cy.get('#fldProfissao').type(gerar.profissao());
+    cy.get('#fldCorPele').type(gerar.corPele());
+    cy.get('#fldGenero').type(gerar.genero(genero));
+    cy.get('#fldOrientacaoSexual').type(orientacaoSexual);
+    cy.get('#fldIdentidadeGenero').type(gerar.identidadePorOrientacao(orientacaoSexual));
+    cy.get('#fldTipoSanguineo').type(gerar.tipoSanguineo());
+    cy.get('#fldDataNascimento').type(gerar.dataNascimento([30,40]));
+
+    // Documentos PJ
+    cy.get('#fldCnpj').type(gerar.cnpj(mascara));
+    cy.get('#fldInscricaoEstadual').type(gerar.inscricaoEstadual(estado) ?? '');
+    cy.get('#fldNomeEmpresa').type(gerar.nomeEmpresa());
+
+    // Documentos PF
+    cy.get('#fldCpf').type(gerar.cpf(mascara, estado));
+    cy.get('#fldCnh').type(gerar.cnh());
+    cy.get('#fldCnhCategoria').type(gerar.cnhCategoria());
+    cy.get('#fldRg').type(gerar.rg(mascara));
+    cy.get('#fldCns').type(gerar.cns(mascara));
+    cy.get('#fldPis').type(gerar.pis(mascara));
+    cy.get('#fldTituloEleitor').type(gerar.tituloEleitor(mascara, estado));
+    cy.get('#fldPassaporte').type(gerar.passaporte());
+    cy.get('#fldCertidaoNascimento').type(gerar.certidao.nascimento(mascara));
+    cy.get('#fldCertidaoCasamento').type(gerar.certidao.casamento(mascara));
+    cy.get('#fldCertidaoObito').type(gerar.certidao.obito(mascara));
+
+    // Contatos
+    cy.get('#fldEmail').type(gerar.email(nomeCompleto));
+    cy.get('#fldDdd').type(gerar.ddd(estado));
+    cy.get('#fldCelular').type(gerar.celular(mascara, estado));
+    cy.get('#fldTelefone').type(gerar.telefone(mascara, estado));
+
+    // Endereços
+    const endereco = gerar.endereco(mascara, estado);
+    cy.get('#fldLogradouro').type(endereco.logradouro);
+    cy.get('#fldNumero').type(endereco.numero);
+    cy.get('#fldComplemento').type(endereco.complemento ? endereco.complemento : '-');
+    cy.get('#fldBairro').type(endereco.bairro);
+    cy.get('#fldLocalidade').type(endereco.localidade);
+    cy.get('#fldEstado').type(endereco.estado);
+    cy.get('#fldCep').type(endereco.cep);
+
+    // Cartão de Crédito
+    const cartaoCredito = gerar.cartaoCredito(mascara, nomeCompleto);
+    cy.get('#fldCartaoCredito').type(cartaoCredito.numero);
+    cy.get('#fldBandeira').type(cartaoCredito.bandeira);
+    cy.get('#fldNomeTitular').type(cartaoCredito.nomeTitular);
+    cy.get('#fldCvv').type(cartaoCredito.cvv);
+    cy.get('#fldDataExpiracao').type(cartaoCredito.dataExpiracao);
+
+    // Conta Bancária
+    const cb = gerar.contaBancaria(banco);
+    cy.get('#fldCodigoBanco').type(cb.codigoBanco);
+    cy.get('#fldNomeBanco').type(cb.nomeBanco);
+    cy.get('#fldAgencia').type(cb.agencia);
+    cy.get('#fldAgenciaDv').type(cb.agenciaDv ? cb.agenciaDv : '-');
+    cy.get('#fldConta').type(cb.conta);
+    cy.get('#fldContaDv').type(cb.contaDv ? cb.contaDv : '-');
+
+    // Banco
+    const bancoObj = gerar.banco(banco);
+    cy.get('#fldBancoCodigo').type(bancoObj.codigoBanco);
+    cy.get('#fldBancoNome').type(bancoObj.nomeBanco);
+    cy.get('#fldRazaoSocial').type(bancoObj.razaoSocial);
+    cy.get('#fldIspb').type(bancoObj.ispb);
+
+    // Veículo
+    cy.get('#fldPlacaAntiga').type(gerar.placaAntiga(mascara));
+    cy.get('#fldPlacaMercosul').type(gerar.placaMercosul(mascara));
+    cy.get('#fldRenavam').type(gerar.renavam());
+
+    // Lorem Ipsum
+    cy.get('#fldPalavra').type(gerar.palavra());
+    cy.get('#fldSentenca').type(gerar.sentenca(15));
+    cy.get('#fldParagrafos').type(gerar.paragrafo(3,8));
+
+    cy.contains('button', 'Enviar').click();
+    cy.wait(2000);
   });
 });
